e7ec99d8c5bda588750a31cbb885fa8ff30f16be
maya-nachiappan <maya.n102304@gmail.com>
2024-09-14 23:56:48 -0700

    declaring the constant min and max values the dice can get

--- CIS25 - assignment 3/main.cpp
+++ CIS25 - assignment 3/main.cpp
@@ -1,14 +1,17 @@
-//
-//  main.cpp
+
 //  CIS25 - assignment 3
 //
 //  Created by maya nachiappan on 9/14/24.
 //
+// a program to roll a dice and get a random value between 1 and 6 each time
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    const int minValue = 1; // minimum dice value
+    const int maxValue =6; // maximum dice value
+    
 }
+
