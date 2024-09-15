2277393e627472e06d9533d11d18983c0d007c73
maya-nachiappan <maya.n102304@gmail.com>
2024-09-15 00:08:10 -0700

    randomize numbers between 1 and 6

--- CIS25 - assignment 3/main.cpp
+++ CIS25 - assignment 3/main.cpp
@@ -6,12 +6,18 @@
 // a program to roll a dice and get a random value between 1 and 6 each time
 
 #include <iostream>
+#include <cstdlib> // header file for rand and srand
 using namespace std;
 
 int main()
 {
     const int minValue = 1; // minimum dice value
     const int maxValue =6; // maximum dice value
+    int random1, random2; // random dice variable values
     
+    random1 = (rand()% (maxValue-minValue + 1)) + minValue;
+    random2 = (rand()% (maxValue-minValue +1)) + minValue;
+    
+    cout << random1 << endl;
+    cout << random2 << endl;
 }
-
